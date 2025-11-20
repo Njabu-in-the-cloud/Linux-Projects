1f23331a90db287324f89ed6de4fdf1881ec0b39
Nonjabulo Kubheka <nontokozok658@gmail.com>
2025-11-20 23:02:06 +0200

    Create temporary user 'yousuf' with expiry
    Set up temporary user 'yousuf' with an expiry date and check details.

--- 02.Temporary user set-up with an expiry/command.sh
+++ 02.Temporary user set-up with an expiry/command.sh
@@ -1,5 +1,4 @@
-ssh root@sever-ip
-sudo vi /etc/ssh/sshd_config
-    PermitRootLogin no
-    Exit :wq
-ssh = sudo systemctl restart sshd
+sudo -i
+useradd -m -s /bin/bash -e 2025-04-15 yousuf
+chage -l yousuf
+ls -ld /home/yousuf
